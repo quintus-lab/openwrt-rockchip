05f806a5137f4e00934cc18bafa12d042f8880dc
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-22 20:12:26 +0800

    backup compile script

---
+++ script/origin-full.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+git clone https://git.openwrt.org/openwrt/openwrt.git
+cd openwrt
+patch -p1 < ../patches/00-R2S.patch
+wget -P target/linux/rockchip/patches-5.4 https://raw.githubusercontent.com/QiuSimons/R2S-OpenWrt/master/PATCH/000-add-nanopi-r2s-support.patch 
+cp -r ../step/* ./
+bash 01-prepare_package.sh
+bash 02-convert_translation.sh
+bash 03-remove_upx.sh
+rm .config
+cp ../seed/origin-full.seed .config
+make defconfig
+make download -j10
+chmod -R 755 ./
+let make_process=$(nproc)+1
+make toolchain/install -j${make_process} V=s
+let make_process=$(nproc)+1
+make -j${make_process} V=s || make -j${make_process} V=s
+cd bin/targets/rockchip/armv8
+gzip -d *.gz
+gzip *.img
