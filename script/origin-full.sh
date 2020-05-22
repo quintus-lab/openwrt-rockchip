0dcc44f70b13c4b38041878a2c076a5260107ef0
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-22 21:19:02 +0800

    update compile script

--- script/origin-full.sh
+++ script/origin-full.sh
@@ -1,15 +1,17 @@
-
 #!/bin/bash
 git clone https://git.openwrt.org/openwrt/openwrt.git
 cd openwrt
-patch -p1 < ../patches/00-R2S.patch
+wget https://github.com/quintuschu/Openwrt-R2S/raw/master/patches/00-R2S.patch
+patch -p1 < ./00-R2S.patch
 wget -P target/linux/rockchip/patches-5.4 https://raw.githubusercontent.com/QiuSimons/R2S-OpenWrt/master/PATCH/000-add-nanopi-r2s-support.patch 
-cp -r ../step/* ./
+wget https://github.com/quintuschu/Openwrt-R2S/raw/master/step/01-prepare_package.sh
+wget https://github.com/quintuschu/Openwrt-R2S/raw/master/step/02-convert_translation.sh
+wget https://github.com/quintuschu/Openwrt-R2S/raw/master/step/03-remove_upx.sh
 bash 01-prepare_package.sh
 bash 02-convert_translation.sh
 bash 03-remove_upx.sh
 rm .config
-cp ../seed/origin-full.seed .config
+wget -O .config https://github.com/quintuschu/Openwrt-R2S/raw/master/seed/origin-full.seed
 make defconfig
 make download -j10
 chmod -R 755 ./
