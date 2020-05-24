d786732f33abc9b25c35af42ed9d130f0b3931b1
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-25 03:21:45 +0800

    fixed oaf compile issue

--- step/01-prepare_package.sh
+++ step/01-prepare_package.sh
@@ -83,6 +83,10 @@ svn co https://github.com/pymumu/smartdns/trunk/package/openwrt package/new/smar
 svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ntlf9t/luci-app-smartdns package/new/smartdns/luci-app-smartdns
 #上网APP过滤
 git clone -b master --single-branch https://github.com/destan19/OpenAppFilter package/new/OpenAppFilter
+pushd package/new/OpenAppFilter
+git checkout a4eb6e3674ca3ab963971d2c9fb52e9419f567d3
+popd
+
 #frp
 git clone https://github.com/lwz322/luci-app-frps.git package/lean/luci-app-frps
 git clone https://github.com/kuoruan/luci-app-frpc.git package/lean/luci-app-frpc
