6d4ed3e7476a5c2accf87af594de8c85cc3c2c14
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-06-03 16:42:18 +0800

    Update 09_load_config.sh

--- r2s_step/09_load_config.sh
+++ r2s_step/09_load_config.sh
@@ -2,7 +2,8 @@
 export TERM=linux
 cd friendlywrt-rk3328
 #deconfig
-wget https://github.com/fanck0605/nanopi-r2s/raw/lean/nanopi-r2_linux_defconfig
+#wget https://github.com/fanck0605/nanopi-r2s/raw/lean/nanopi-r2_linux_defconfig
+wget https://github.com/fanck0605/nanopi-r2s/raw/openwrt-lienol/nanopi-r2_linux_defconfig
 cat ./nanopi-r2_linux_defconfig > ./kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
 echo '
 CONFIG_CC_DISABLE_WARN_MAYBE_UNINITIALIZED=y
