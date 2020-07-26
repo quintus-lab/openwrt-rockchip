4690ddaf90126bf9035be6443704facf134927e5
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-07-26 18:46:00 +0800

    Update 01-prepare_package.sh

--- step/01-prepare_package.sh
+++ step/01-prepare_package.sh
@@ -205,7 +205,9 @@ rm -f ./package/feeds/packages/frp
 git clone https://github.com/lwz322/luci-app-frps.git package/lean/luci-app-frps
 git clone https://github.com/kuoruan/luci-app-frpc.git package/lean/luci-app-frpc
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/frp packages/lean/frp
-svn co https://github.com/openwrt/packages/trunk/net/frp package/feeds/packages/frp
+#svn co https://github.com/openwrt/packages/trunk/net/frp package/feeds/packages/frp
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/frp package/feeds/packages/frp
+
 #onliner
 svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ctcgfw/luci-app-onliner package/ctcgfw/luci-app-onliner
 #filetransfer
