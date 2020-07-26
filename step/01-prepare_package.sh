6efed340f41f9e5979f19a1ea2ed8ac884e9eba0
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-07-27 02:40:26 +0800

    test frp source

--- step/01-prepare_package.sh
+++ step/01-prepare_package.sh
@@ -205,8 +205,8 @@ wget -O package/lean/luci-app-jd-dailybonus/root/usr/share/jd-dailybonus/JD_Dail
 #rm -f ./feeds/luci/applications/luci-app-frpc
 #rm -rf ./feeds/packages/net/frp
 #rm -f ./package/feeds/packages/frp
-git clone https://github.com/lwz322/luci-app-frps.git package/lean/luci-app-frps
-git clone https://github.com/kuoruan/luci-app-frpc.git package/lean/luci-app-frpc
+#git clone https://github.com/lwz322/luci-app-frps.git package/lean/luci-app-frps
+#git clone https://github.com/kuoruan/luci-app-frpc.git package/lean/luci-app-frpc
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/frp packages/lean/frp
 #svn co https://github.com/openwrt/packages/trunk/net/frp package/feeds/packages/frp
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/frp package/feeds/packages/frp
